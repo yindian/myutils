8f37f29755b17ff432a94e1623319bdc5806d7c8
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2009-05-29 17:36:41 +0000

    update new jigen diff & script. done

--- scripts/python/newdict/jigen/jigenxml2st.py
+++ scripts/python/newdict/jigen/jigenxml2st.py
@@ -34,14 +34,128 @@ except:
 		else:
 			file.write(ustr.encode(fnenc, 'replace'))
 
+numhandict = {
+		1:  u'一',
+		2:  u'二',
+		3:  u'三',
+		4:  u'四',
+		5:  u'五',
+		6:  u'六',
+		7:  u'七',
+		8:  u'八',
+		9:  u'九',
+		10: u'十',
+	}
+
+def num2Han(num):
+	assert num > 0 and num < 100
+	if num <= 10:
+		return numhandict[num]
+	elif num < 20:
+		return numhandict[10] + numhandict[num-10]
+	elif num % 10 == 0:
+		return numhandict[num / 10] + numhandict[10]
+	else:
+		return numhandict[num / 10] + numhandict[10] + numhandict[num % 10]
+
+kaeriten = {
+		u'レ':  u"\u3191",
+		u'一':  u"\u3192",
+		u'二':  u"\u3193",
+		u'三':  u"\u3194",
+		u'四':  u"\u3195",
+		u'上':  u"\u3196",
+		u'中':  u"\u3197",
+		u'下':  u"\u3198",
+		u'甲':  u"\u3199",
+		u'乙':  u"\u319A",
+		u'丙':  u"\u319B",
+		u'丁':  u"\u319C",
+	}
+
+def getWordContent(child2):
+	assert child2.tagName == u'熟語'
+	infoList = []
+	sWord = None
+	for child3 in child2.childNodes:
+		if (child3.nodeType != Node.ELEMENT_NODE):
+			continue;
+		sTagName3 = (child3).tagName;
+		if (sTagName3 == (u"見出語")):
+			sWord = child3.textContent.strip();
+			try:
+				assert len(sWord) >= 2
+			except:
+				print >> sys.stderr, 'Word = ',
+				writeunicode(sWord, sys.stderr)
+				raise
+			sValue = []
+			for child4 in child3.childNodes:
+				if (child4.nodeType == Node.ELEMENT_NODE):
+					assert child4.tagName == u"返点"
+					sType = (child4).getAttribute(u"type");
+					for j in range(len(sType)):
+						sValue += [kaeriten[sType[j]]];
+				elif child4.nodeType == Node.TEXT_NODE:
+					sValue += [child4.textContent.strip()];
+			infoList.append(u"【熟語】" + u''.join(sValue));
+		elif (sTagName3 == (u"音")):
+			infoList.append(u"【音】" + child3.textContent.strip());
+		elif (sTagName3 == (u"解")):
+			sValue = [];
+			for child5 in child3.childNodes:
+				if (child5.nodeType != Node.ELEMENT_NODE):
+					continue;
+				sTagName5 = (child5).tagName;
+				if (sTagName5 == (u"號")):
+					sValue += [child5.textContent];
+				elif (sTagName5 == (u"義")):
+					sValue2 = [];
+					# <音>
+					# <標識>
+					# <返点 type=u"[レ一二三上中下]|一レ|上レ">
+					for child6 in child5.childNodes:
+						if child6.nodeType == Node.ELEMENT_NODE:
+							sTagName6 = (child6).tagName;
+							if (sTagName6 == (u"音")):
+								sValue2 += [u"(" , child6.textContent , u")"];
+							elif (sTagName6 == (u"標識")):
+								sValue2 += [u"[" , child6.textContent , u"]"];
+							elif (sTagName6 == (u"返点")):
+								sType = (child6).getAttribute(u"type");
+								for j in range(len(sType)):
+									sValue2 += [kaeriten[sType[j]]];
+							else:
+								raise
+						elif child6.nodeType == Node.TEXT_NODE:
+							sValue2 += [child6.textContent.strip()];
+						else:
+							raise
+					sValue += [u''.join(sValue2) , u"\n"];
+			infoList.append(u"【解】\n" + u''.join(sValue));
+		else:
+			print >> sys.stderr, 'Tagname3 = ',
+			writeunicode(sTagName3, sys.stderr)
+			raise
+	assert sWord is not None
+	return (sWord, infoList)
+	
+
 assert __name__ == '__main__'
 
+resultList = []
 characterMap = {};
+radicalMap = {}
 NUM_RADICALS = 214
+
+charCount = 0
+wordCount = 0
 
 for i in range(NUM_RADICALS):
 	document = libxml2dom.parseString(open('%03d.xml' % (i+1), 'r').read());
 	sRadical = None;
+	radicalList = []
+	lastStrokes = -1
 	print >> sys.stderr, 'Processing %03d.xml' % (i+1)
 	for child in document.getElementsByTagName(u'部首')[0].childNodes:
 		if (child.nodeType != Node.ELEMENT_NODE):
@@ -54,6 +168,7 @@ for i in range(NUM_RADICALS):
 				print >> sys.stderr, 'Len not 2: ',
 				writeunicode(child.textContent, sys.stderr)
 			sRadical = u"" + child.textContent[0];
+			radicalList.append(u"【部首】" + sRadical + u"  番号%d" % (i+1));
 		elif (sTagName == (u"筆畫")):
 			strokes = -1;
 			for child2 in child.childNodes:
@@ -62,9 +177,20 @@ for i in range(NUM_RADICALS):
 				sTagName2 = (child2).tagName;
 				if (sTagName2 == (u"畫數")):
 					strokes = int(child2.textContent);
+					try:
+						assert strokes > lastStrokes
+					except:
+						print >> sys.stderr, 'Strokes = %d, last strokes = %d' % (strokes, lastStrokes)
+						raise
+					lastStrokes = strokes
+					if strokes > 0:
+						radicalList.append(u"\n\n筆畫" + num2Han(strokes) + u"\n");
+					else:
+						radicalList.append(u"\n\n");
 				elif (sTagName2 == (u"漢字")):
 					sCharacter = None;
 					codePoint = 0;
+					wordList = [];
 					
 					infoList = [];
 					infoList.append(u"【部首】" + sRadical);
@@ -90,6 +216,8 @@ for i in range(NUM_RADICALS):
 											print >> sys.stderr, 'sCharacter = ',
 											writeunicode(sCharacter, sys.stderr)
 											raise
+							infoList.append(u"【親字】" + sCharacter);
+							radicalList.append(u" " + sCharacter);
 						elif (sTagName3 == (u"字解")):
 							for child4 in child3.childNodes:
 								if (child4.nodeType != Node.ELEMENT_NODE):
@@ -145,18 +273,8 @@ for i in range(NUM_RADICALS):
 														sValue2 += [u"[" , child6.textContent , u"]"];
 													elif (sTagName6 == (u"返点")):
 														sType = (child6).getAttribute(u"type");
-														tmpdict = {
-																u'レ':  u"\u3191",
-																u'一':  u"\u3192",
-																u'二':  u"\u3193",
-																u'三':  u"\u3194",
-																u'四':  u"\u3195",
-																u'上':  u"\u3196",
-																u'中':  u"\u3197",
-																u'下':  u"\u3198",
-															}
 														for j in range(len(sType)):
-															sValue2 += [tmpdict[sType[j]]];
+															sValue2 += [kaeriten[sType[j]]];
 													else:
 														raise
 												elif child6.nodeType == Node.TEXT_NODE:
@@ -164,13 +282,14 @@ for i in range(NUM_RADICALS):
 												else:
 													raise
 											sValue += [u''.join(sValue2) , u"\n"];
-									infoList.append(u"【解】" + u''.join(sValue));
+									infoList.append(u"【解】\n" + u''.join(sValue));
 								elif (sTagName4 == (u"解字")):
 									# <標識>
 									sValue = [];
 									for child5 in child4.childNodes:
 										if child5.nodeType == Node.ELEMENT_NODE:
-											if (sTagName == (u"標識")):
+											sTagName5 = child5.tagName
+											if (sTagName5 == (u"標識")):
 												sValue += [u"[" , child5.textContent , u"]"];
 										elif child5.nodeType == Node.TEXT_NODE:
 											sValue += [child5.textContent.strip()];
@@ -185,14 +304,17 @@ for i in range(NUM_RADICALS):
 									sValue = [];
 									for child5 in child4.childNodes:
 										if child5.nodeType == Node.ELEMENT_NODE:
-											if (sTagName == (u"標識")):
+											sTagName5 = child5.tagName
+											if (sTagName5 == (u"標識")):
 												sValue += [u"[" , child5.textContent , u"]"];
-											elif (sTagName == (u"訓")):
+											elif (sTagName5 == (u"訓")):
 												sValue += [u"「" , child5.textContent , u"」"];
-											elif (sTagName == (u"同訓字")):
+											elif (sTagName5 == (u"同訓字")):
 												sValue += [child5.textContent];
-											elif (sTagName == (u"同訓解")):
+											elif (sTagName5 == (u"同訓解")):
 												sValue += [u'\n', child5.textContent];
+											else:
+												raise
 										elif child5.nodeType == Node.TEXT_NODE:
 											sValue += [child5.textContent.strip()];
 										else:
@@ -203,7 +325,7 @@ for i in range(NUM_RADICALS):
 									writeunicode(sTagName4, sys.stderr)
 									raise
 						elif (sTagName3 == (u"熟語")):
-							pass
+							wordList.append(getWordContent(child3));
 						else:
 							print >> sys.stderr, 'Tagname3 = ',
 							writeunicode(sTagName3, sys.stderr)
@@ -212,14 +334,35 @@ for i in range(NUM_RADICALS):
 						writeunicode(u'%03d' % (i+1) + u", radical:" + sRadical + u", strokes:" + repr(strokes) + u" codePoint==0  char=" + sCharacter, sys.stderr);
 					else:
 						if (characterMap.get(sCharacter) is not None):
-							writeunicode(u'%03d' % (i+1) + u", radical:" + sRadical + u", strokes:" + repr(strokes) + u" " + sCharacter + ' dup with ' + u''.join(characterMap[sCharacter][:2]), sys.stderr);
-						characterMap.__setitem__(sCharacter, infoList);
+							writeunicode(u'%03d' % (i+1) + u", radical:" + sRadical + u", strokes:" + repr(strokes) + u" " + sCharacter + ' dup with ' + u''.join(resultList[sCharacter][:2]), sys.stderr);
+					resultList.append((sCharacter, infoList));
+					resultList.extend(wordList)
+					charCount += 1
+					wordCount += len(wordList)
 				else:
-					pass
+					raise
+	radicalMap[i] = u''.join(radicalList)
 
-sys.exit(0)
-
-for key, value in characterMap.items():
+for key, value in resultList:
 	writeunicode(key, sys.stdout, False)
 	print '\t',
-	writeunicode(u'\n'.join(value).replace('\n', '\\n'))
+	if value[0].startswith(u'【部首】') and value[1].startswith(u'【畫數】'):
+		try:
+			assert value[2].startswith(u'【親字】')
+		except:
+			print >> sys.stderr, 'Word = ',
+			writeunicode(key, sys.stderr)
+			print >> sys.stderr, ' Line = ',
+			writeunicode(value[2], sys.stderr)
+			raise
+		value = u' '.join(value[:3]) + u'\n' + u'\n'.join(value[3:])
+	else:
+		value = u'\n'.join(value)
+	writeunicode(value.replace('\n', '\\n'))
+for i in range(NUM_RADICALS):
+	writeunicode(u'KO字源:' + unichr(0x2f00 + i) + u'部', sys.stdout, False)
+	print '\t',
+	writeunicode(radicalMap[i].replace(u'\n', u'\\n'))
+print >> sys.stderr, 'Total characters:', charCount
+print >> sys.stderr, 'Total words:', wordCount
+print >> sys.stderr, 'Total radicals:', NUM_RADICALS
