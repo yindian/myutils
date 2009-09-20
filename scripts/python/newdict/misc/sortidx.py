968baae6ea6be1cb370c027dd6c57eb1a90a4ac4
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2009-09-20 14:06:05 +0000

    update misc scripts

--- scripts/python/newdict/misc/sortidx.py
+++ scripts/python/newdict/misc/sortidx.py
@@ -4,7 +4,7 @@ import sys, re, os.path, struct, os
 
 def showhelp():
 	print "Usage: %s somedict.ifo" % os.path.basename(sys.argv[0])
-	print "Generate .pos file for S60Dict from .idx file"
+	print "Re-sort Stardict .idx index file"
 
 assert __name__ == '__main__'
 
@@ -36,17 +36,36 @@ reidx = re.compile(r'.*?\x00.{8}', re.S)
 idxmatches = reidx.findall(idxcontent)
 #print len(idxmatches)
 assert len(idxmatches) == wordcount
+idxlist = []
+
+for record in idxmatches:
+	p = record.find('\x00') + 1
+	ar = struct.unpack('!%dc2L' % p, record)
+	word = ''.join(ar[:-3])
+	idxlist.append((unicode(word, 'latin-1').lower().encode('latin-1'),
+		word, ar[-2], ar[-1]))
+	#try:
+	#	print unicode(idxlist[-1][0], 'utf-8').encode('gbk', 'replace'), idxlist[-1][1:]
+	#except:
+	#	print idxlist[-1]
+	#	raise
+
+
+print >> sys.stderr, 'Generating...'
+
+idxlist.sort()
+
+result = []
+for tmp, word, offset, length in idxlist:
+	result.append(word)
+	result.append('\0')
+	result.append(struct.pack('!2L', offset, length))
+result = ''.join(result)
 
 print >> sys.stderr, 'Writing...'
-f = open(base + '.pos', 'wb')
-pos = 0
-for record in idxmatches:
-	p = record.find('\x00')
-	if p >= 192:
-		print >> sys.stderr, 'Warning: keyword longer than 200 bytes:',
-		print >> sys.stderr, record[:p]
-	f.write(struct.pack('<L', pos))
-	pos += len(record)
+
+f = open(base + '.idx.new', 'wb')
+f.write(result)
 f.close()
 
 print >> sys.stderr, 'Done'
