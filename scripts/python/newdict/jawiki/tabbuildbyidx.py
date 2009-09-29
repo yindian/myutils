069ec873db83c3425d9f1c67d425223695beca03
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2009-09-29 16:14:32 +0000

    update large dict build script

--- scripts/python/newdict/jawiki/tabbuildbyidx.py
+++ scripts/python/newdict/jawiki/tabbuildbyidx.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
 	f = open(sys.argv[1], 'r')
 	lines = f.readlines()
 	f.close()
-	lines.sort()
 	#lines = [line.split('\t') for line in lines]
 	items = []
 	for line in lines:
 		line = line.split('\t')
-		items.append([line[0], int(line[1], 16)])
+		items.append([line[0].lower(), line[0], int(line[1], 16)])
 	del lines
+	items.sort()
 
 	print >> sys.stderr, 'Sorting done. Now building dic'
 	f = open(sys.argv[2], 'rb')
@@ -29,11 +29,14 @@ if __name__ == '__main__':
 	f = open(sys.argv[2], 'rb')
 	ifofile = open('out.ifo', 'wb')
 	idxfile = open('out.idx', 'wb')
-	dicfile = open('out.dic', 'wb')
+	dicfile = open('out.dict', 'wb')
 
+	itemnum = 0
 	for item in items:
-		f.seek(item[1])
-		line = f.readline()[:deleol].split('\t')
+		f.seek(item[-1])
+		line = f.readline()[:deleol].split('\t', 1)
+		if not line[0] or not line[1]: continue
+		itemnum += 1
 		line[1] = line[1].replace('<br>\\n', '\n')
 		offset_old = dicfile.tell()
 		dicfile.write(line[1])
@@ -41,7 +44,7 @@ if __name__ == '__main__':
 		idxfile.write('\0')
 		idxfile.write(struct.pack('>LL', offset_old, len(line[1])))
 	f.close()
-	ifofile.write("StarDict's dict ifo file\nversion=2.4.2\nwordcount=%d\nidxfilesize=%ld\nbookname=%s\nsametypesequence=m\n" % (len(items), idxfile.tell(), sys.argv[0]+' output'))
+	ifofile.write("StarDict's dict ifo file\nversion=2.4.2\nwordcount=%d\nidxfilesize=%ld\nbookname=%s\nsametypesequence=m\n" % (itemnum, idxfile.tell(), sys.argv[0]+' output'))
 	ifofile.close()
 	idxfile.close()
 	dicfile.close()
