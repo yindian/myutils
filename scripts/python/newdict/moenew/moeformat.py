0158f3e1bc74324cc2a28cd0624e4a6c711b3f7a
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2009-11-08 06:28:50 +0000

    update moe gaiji and format script

---
+++ scripts/python/newdict/moenew/moeformat.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+	import psyco
+	psyco.full()
+except:
+	pass
+import sys
+gaijimap = {}
+f = open('gaiji.txt', 'r')
+for line in f:
+	line = line[:-1].split()
+	assert len(line) == 4
+	src = int(line[0], 16)
+	dest = line[-1]
+	if dest.find('#') >= 0:
+		dest = dest[:dest.find('#')]
+	assert src and not gaijimap.has_key(src)
+	try:
+		assert dest and dest.count('|') <= 1
+		if dest[0] == '{':
+			assert dest[-1] == '}' and dest.count('{') == dest.count('}') == 1
+	except:
+		print >> sys.stderr, hex(src), unicode(dest, 'utf-8').encode('gbk', 'replace')
+		raise
+	gaijimap[src] = unicode(dest, 'utf-8')
+f.close()
+
+def fwalpha2hw(str):
+	result = []
+	for c in str:
+		if 0xFF21 <= ord(c) <= 0xFF3A or 0xFF41 <= ord(c) <= 0xFF5A:
+			result.append(unichr(ord(c) - 0xFF00 + 0x20))
+		else:
+			result.append(c)
+	return u''.join(result)
+
+def subsgaiji(str):
+	ar = str.split(u'<?')
+	result = [ar[0]]
+	for s in ar[1:]:
+		p = s.index(u'>')
+		code = int(s[:p], 16)
+		result.append(gaijimap[code])
+		result.append(s[p+1:])
+	return u''.join(result)
+
+def substitlegaiji(str):
+	ar = str.split(u'<?')
+	result1 = [ar[0]]
+	result2 = [ar[0]]
+	for s in ar[1:]:
+		p = s.index(u'>')
+		code = int(s[:p], 16)
+		dest = gaijimap[code]
+		if dest[0] == u'{':
+			p = dest.find(u'|')
+			if p < 0:
+				assert len(dest) > 3
+				result1.append(dest)
+				result2.append(dest)
+			else:
+				result1.append(dest[1:p])
+				other = dest[p+1:-1]
+				p = other.find(u'?')
+				assert p != 0
+				if p > 0:
+					other = other[:p]
+				if len(other) <= 1:
+					assert other
+					result2.append(other)
+				else:
+					result2.append(u'{')
+					result2.append(other)
+					result2.append(u'}')
+		else:
+			result1.append(dest)
+			result2.append(dest)
+		p = s.index(u'>')
+		result1.append(s[p+1:])
+		result2.append(s[p+1:])
+	result1 = u''.join(result1)
+	result2 = u''.join(result2)
+	if result1 == result2:
+		return result1
+	else:
+		return u'|'.join([result1, result2])
+
+for line in sys.stdin:
+	line = unicode(line[:-1], 'utf-8')
+	word, mean = line.split(u'\t')
+	try:
+		word = substitlegaiji(word)
+	except:
+		print >> sys.stderr, word.encode('gbk', 'replace')
+		raise
+	means = mean.split(u'\\n')
+	for i in range(len(means)):
+		mean = means[i]
+		#if mean.startswith(u'注音一式:') or mean.startswith(u'注音二式:') or mean.startswith(u'通用拼音:') or mean.startswith(u'詞目:') or mean.startswith(u'相似詞:') or mean.startswith(u'相反詞:'):
+		#	mean = mean + u'\\n'
+		#elif mean.startswith(u'注釋:'):
+		#	pass
+		#elif mean.lstrip().startswith(u'<?FE') or (mean.startswith(u'（') and mean.endswith(u'）')) or mean.startswith(u'資料來源:'):
+		#	mean = u'\\n' + mean
+		#else:
+		#	#try:
+		#	#	assert i == 0 or means[i-1].startswith(u'注釋:') or not mean or mean[0].isspace()
+		#	#except:
+		#	#	print >> sys.stderr, word.encode('gbk', 'replace'), mean.encode('gbk', 'replace')
+		#	#	pass
+		#	mean = mean.lstrip()
+		mean = subsgaiji(mean)
+		#if mean.startswith(u'注音一式:') or mean.startswith(u'注音二式:') or mean.startswith(u'通用拼音:'):
+		if not mean.startswith(u'注音一式:'):
+			mean = fwalpha2hw(mean)
+		else:
+			mean = mean.replace(u'|', u'\uff5c')
+		means[i] = mean
+	mean = u'\\n'.join(means)
+	print (word + u'\t' + mean).encode('utf-8')
