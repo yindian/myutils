3a2dfc377b65788890399f77885ef2d4888af84c
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2010-06-15 06:46:10 +0000

    update moe dict scripts

---
+++ trunk/scripts/python/newdict/moenew/new/moetab2epw.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: sjis -*-
+import sys, unicodedata
+import pdb, traceback
+
+def htmlquote(str):
+	return str.replace('&', '&amp;').replace('&amp;#', '&#').replace(
+			'<', '&lt;').replace('>', '&gt;').replace('"', '&quot;'
+					).replace("'", '&apos;')
+
+_hatmap = {
+u'\uE29C': u'a',
+u'\uE29D': u'a',
+u'\uE29E': u'a',
+u'\uE29F': u'a',
+u'\uE2A0': u'e',
+u'\uE2A1': u'e',
+u'\uE2A2': u'e',
+u'\uE2A3': u'e',
+u'\uE2A4': u'i',
+u'\uE2A5': u'i',
+u'\uE2A6': u'i',
+u'\uE2A7': u'i',
+u'\uE2A8': u'o',
+u'\uE2A9': u'o',
+u'\uE2AA': u'o',
+u'\uE2AB': u'o',
+u'\uE2AC': u'r',
+u'\uE2AD': u'r',
+u'\uE2AE': u'r',
+u'\uE2AF': u'r',
+u'\uE2B0': u'u',
+u'\uE2B1': u'u',
+u'\uE2B2': u'u',
+u'\uE2B3': u'u',
+u'\uE2B4': u'z',
+u'\uE2B5': u'z',
+u'\uE2B6': u'z',
+u'\uE2B7': u'z',
+u'\uEB7C': u'e',
+u'\uEC1F': u'a',
+u'\uEC20': u'e',
+u'\uEC21': u'i',
+u'\uEC22': u'o',
+u'\uEC23': u'u',
+		}
+def unhat(str, hatmap=_hatmap):
+	ar = str.split('&#x')
+	result = [ar[0].decode('sjis')]
+	for s in ar[1:]:
+		p = s.index(';')
+		result.append(unichr(int(s[:p], 16)))
+		if hatmap.has_key(result[-1]):
+			result[-1] = hatmap[result[-1]]
+		result.append(s[p+1:].decode('sjis'))
+	str = u''.join(result).replace(u'\u3000', u' ')
+	if str.find(u'(') > 0:
+		assert str[str.find(u'('):].startswith(u'(變)')
+		str = str[:str.index(u'(')]
+	elif str.find(u'（') > 0:
+		assert str[str.find(u'（')+2:].startswith(u'音）')
+		str = str[:str.index(u'（')]
+	return str.strip().encode('ascii').replace(' ', '')
+
+assert __name__ == '__main__'
+
+print """\
+<html>
+<head>
+<meta http-equiv="Content-Type" charset="Shift_JIS">
+<title>教育部重編国語辞典修訂本</title>
+</head>
+<body>"""
+
+lineno = 0
+for line in open(sys.argv[1]):#sys.stdin:
+	lineno += 1
+	word, mean = line[:-1].split('\t', 1)
+	pinyin = ''
+	ar = htmlquote(mean).split('\\n')
+	if len(ar) > 3 and ar[3].startswith('通用'):
+		pinyin = ar[3][ar[3].index(':')+1:]
+		try:
+			pinyin = unhat(pinyin)
+		except:
+			traceback.print_exc()
+			pdb.set_trace()
+			raise
+		print '<dl><dt id="m%d">%s</dt><key type="表記">%s</key><dd>%s</dd></dl>' % (lineno, word, pinyin, '<br>'.join(ar))
+	else:
+		print '<dl><dt id="m%d">%s</dt><dd>%s</dd></dl>' % (lineno, word, '<br>'.join(ar))
+
+print """\
+</body>
+</html>"""
