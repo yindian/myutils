1a88e8b54f910dc5c25ecf9a24e561162f174fec
yindian <yindian@81255370-523a-0410-a590-15e8afa08e71>
2011-06-26 02:09:22 +0000

    add captcha handling

--- scripts/python/gmdown.py
+++ scripts/python/gmdown.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys, os, urllib, urllib2
+import re, time, glob, traceback
 
+pathspecial = re.compile(r'[/\\;:<>\?\*&|]')
+pathspecial = re.compile(r'[/\\:]')
 def fnquote(s):
-	return s.replace('/', '_').replace('\\', '_').replace(':', '_')
+	return pathspecial.sub('_', s)
 
 assert __name__ == '__main__'
 
@@ -10,21 +14,65 @@ maxnum = len(sys.argv) - 1
 digits = maxnum > 99 and 3 or (maxnum > 9 and 2 or 1)
 fnfmt = '%%0%dd. %%s.mp3' % (digits,)
 idx = 0
+failids = []
+site = 'aHR0cDovL3d3dy5nb29nbGUuY24='.decode('base64')
+prefix = 'aHR0cDovL3d3dy5nb29nbGUuY24vbXVzaWMvdG9wMTAwL211c2ljZG93bmxvYWQ/aWQ9'.decode('base64')
 for sid in sys.argv[1:]:
 	idx += 1
-	f = urllib2.urlopen('http://www.google.cn/music/top100/musicdownload?id=' + sid)
+	if glob.glob(fnfmt % (idx, '*')):
+		print 'Skipping index', idx
+		continue
+	f = urllib2.urlopen(prefix + sid)
 	buf = f.read()
 	f.close()
-	front = buf.index('<a href="/music/top100/url?q=http://') + 9
-	last = buf.index('"><img src="', front)
-	url = 'http://www.google.cn' + buf[front:last].replace('%3A', ':'
-	).replace('%2F', '/').replace('&amp;', '&')
-	front = buf.index('<tr class="meta-data-tr"><td class="td-song-name">') + 50
-	last =buf.index('</td>', front)
-	title = buf[front:last]
-	print 'Downloading', fnfmt % (idx, title)
-	f = urllib2.urlopen(url)
-	g = open(fnquote(fnfmt % (idx, title)), 'wb')
-	g.write(f.read())
-	f.close()
-	g.close()
+	try:
+		front = buf.index('<a href="/music/top100/url?q=http://') + 9
+		last = buf.index('"><img src="', front)
+		url = site + buf[front:last].replace('%3A', ':'
+		).replace('%2F', '/').replace('&amp;', '&')
+		front = buf.index('<tr class="meta-data-tr"><td class="td-song-name">') + 50
+		last =buf.index('</td>', front)
+		title = buf[front:last]
+	except:
+		try:
+			assert buf.find('captcha') > 0
+			front = buf.index('<div class="captchaImage"><img src="') + 36
+			end = buf.index('"', front)
+			url = site + buf[front:end]
+			captcha = raw_input('Please input captcha text for ' + url + ' : ')
+			front = buf.index('<input type="hidden" name="tok" value="', front) + 39
+			end = buf.index('"', front)
+			tok = buf[front:end]
+			f = urllib2.urlopen(prefix + sid + '&tok=' + tok + '&response=' + captcha + '&submitBtn=' + urllib.quote('提交'))
+			buf = f.read()
+			f.close()
+			front = buf.index('<a href="/music/top100/url?q=http://') + 9
+			last = buf.index('"><img src="', front)
+			url = site + buf[front:last].replace('%3A', ':'
+			).replace('%2F', '/').replace('&amp;', '&')
+			front = buf.index('<tr class="meta-data-tr"><td class="td-song-name">') + 50
+			last =buf.index('</td>', front)
+			title = buf[front:last]
+		except:
+			traceback.print_exc()
+			f = open('musicdownload' + sid, 'w')
+			f.write(buf)
+			f.close()
+			failids.append(sid)
+			print >> sys.stderr, 'Failed for', sid
+			time.sleep(5)
+			continue
+	fname = fnquote(fnfmt % (idx, title))
+	if not os.path.exists(fname):
+		print 'Downloading', fname
+		f = urllib2.urlopen(url)
+		g = open(fname, 'wb')
+		g.write(f.read())
+		f.close()
+		g.close()
+	else:
+		print 'Skipping', fname
+		time.sleep(2)
+f = open('fail.txt', 'w')
+f.write('\n'.join(failids))
+f.close()
