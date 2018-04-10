4dcc2261f6764b4cf086cb8c627aaa861fa4e3e1
Yin Dian <yindian@gmail.com>
2018-04-10 13:03:05 +0800

    rewrite algorithm for ip merge script

--- scripts/python/ipmerge.py
+++ scripts/python/ipmerge.py
@@ -29,7 +29,10 @@ for line in f:
         continue
     try:
         ip, cnt = line.split()
-        cnt = int(cnt)
+        try:
+            cnt = int(cnt)
+        except:
+            cnt = float(cnt)
         ip = socket.inet_aton(ip)
         ip = struct.unpack('!L', ip)[0]
     except:
@@ -41,127 +44,22 @@ for line in f:
     except KeyError:
         d[ip] = cnt
 l = d.keys()
-for k in d.iterkeys():
-    d[k] = (32, d[k], 0.) # mask, count, 1 - coverage
-while len(d) > max_ips:
-    ar = d.keys()
-    ar.sort()
-    br = []
-    t = {}
-    sm = set()
-    for i in xrange(len(ar)):
-        ip = ar[i]
-        mask = d[ip][0]
-        assert mask
-        mask -= 1
-        ip &= (1 << 32) - (1 << (32 - mask))
-        ret = None
-        while mask in sm:
-            if t.has_key((ip, mask)):
-                ret = t[(ip, mask)]
-                break
-            ip &= (1 << 32) - (1 << (32 - mask))
-            mask -= 1
-        if not ret:
-            ip = ar[i]
-            mask, count, penalty = d[ip]
-            mask -= 1
-            unmask = 1 << (32 - mask)
-            ip &= (1 << 32) - unmask
-            penalty /= 2.
-            if ip < ar[i]:
-                ip_next = ar[i]
-                j = i - 1
-                while j >= 0:
-                    try:
-                        ip2 = ar[j]
-                    except IndexError:
-                        break
-                    if ip2 & ((1 << 32) - unmask) != ip:
-                        break
-                    mask2, count2, penalty2 = d[ip2]
-                    count += count2
-                    penalty += (ip_next - ip2 - (1 << (32 - mask2))) * 1. / unmask
-                    penalty += penalty2 / (1 << (mask2 - mask))
-                    ip_next = ip2
-                    j -= 1
-                penalty += (ip_next - ip) * 1. / unmask
-                k = j + 1
-            else:
-                k = i
-            ip_next = ar[i] + (unmask >> 1)
-            j = i + 1
-            while True:
-                try:
-                    ip2 = ar[j]
-                except IndexError:
-                    break
-                if ip2 & ((1 << 32) - unmask) != ip:
-                    break
-                mask2, count2, penalty2 = d[ip2]
-                count += count2
-                penalty += (ip2 - ip_next) * 1. / unmask
-                penalty += penalty2 / (1 << (mask2 - mask))
-                ip_next = ip2 + (1 << (32 - mask2))
-                j += 1
-            penalty += (ip + unmask - ip_next) * 1. / unmask
-            ret = t[(ip, mask)] = (k, j - k, ip, mask, count, penalty, penalty / (j - k))
-            if mask not in sm:
-                sm.add(mask)
-                while mask < 32:
-                    mask += 1
-                    sm.add(mask)
-        br.append(ret)
-    min_i = -1
-    min_score = math.exp(32)
-    score_field = -1
-    for i in xrange(len(ar)):
-        if br[i][1] < 2:
-            continue
-        score = br[i][score_field]
-        if score < min_score:
-            min_i = i
-            min_score = score
-    if min_i < 0:
-        max_count = max([x[-3] for x in br])
-        max_count = math.log(max_count)
-        for i in xrange(len(ar)):
-            score = br[i][-2] / math.log(br[i][-3] + 0.1) * max_count * (math.exp(32) / math.exp(br[i][-4])) ** 0.5
-            if score < min_score:
-                min_i = i
-                min_score = score
-    assert min_i >= 0
-    min_br = br[min_i]
-    #print >> sys.stdout, min_i, len(d), min_score, '%s/%d' % (socket.inet_ntoa(struct.pack('!L', min_br[2])), min_br[3]), min_br, ' '.join(['%s/%d' % (socket.inet_ntoa(struct.pack('!L', ar[i])), d[ar[i]][0]) for i in xrange(min_br[0], min_br[0] + min_br[1])])
-    if min_br[3] < min_mask:
-        break
-    for i in xrange(min_br[0], min_br[0] + min_br[1]):
-        del d[ar[i]]
-    d[min_br[2]] = min_br[3:6]
+l.sort()
+r = set([(0, 0)])
+rm = 0
 
-if True:
-    l.sort()
-    to_del = []
-    for k in d.keys():
-        ip = k
-        mask, count, penalty = d[ip]
-        unmask = 1 << (32 - mask)
-        ips = set()
-        for i in xrange(bisect_left(l, ip), len(l)):
-            if l[i] & ((1 << 32) - unmask) == ip:
-                ips.add(l[i])
-            else:
-                break
-        assert len(ips)
+def shrink_subnet(ips, ip, mask):
+    if True:
         while mask < 32:
             mask += 1
             unmask = 1 << (32 - mask)
             ip2 = ip + unmask
+            unmask = (1 << 32) - unmask
             ok1 = ok2 = True
             for x in ips:
-                if x & ((1 << 32) - unmask) != ip:
+                if x & unmask != ip:
                     ok1 = False
-                if x & ((1 << 32) - unmask) != ip2:
+                if x & unmask != ip2:
                     ok2 = False
                 if not (ok1 or ok2):
                     break
@@ -172,22 +70,73 @@ if True:
             else:
                 mask -= 1
                 break
-        if ip == k and mask != d[k][0]:
-            d[ip] = mask, count, 1. - len(ips) * 1. / (1 << (32 - mask)), ips
-        elif ip != k:
-            assert not d.has_key(ip)
-            to_del.append(k)
-            d[ip] = mask, count, 1. - len(ips) * 1. / (1 << (32 - mask)), ips
+    return ip, mask, len(ips) * 1. / (1 << (32 - mask))
+
+while len(r) < max_ips or rm < min_mask:
+    to_del = []
+    s = set([x[0] for x in r if x[1] == rm])
+    assert s
+    chosen_ip = None
+    chosen_ip1 = chosen_mask1 = None
+    chosen_ip2 = chosen_mask2 = None
+    max_score = -1.
+    for ip in s:
+        mask = rm
+        unmask = 1 << (32 - mask)
+        unmask = (1 << 32) - unmask
+        ips = set()
+        for i in xrange(bisect_left(l, ip), len(l)):
+            if l[i] & unmask == ip:
+                ips.add(l[i])
+            else:
+                break
+        assert len(ips)
+        coverage = len(ips) * 1. / (1 << (32 - mask))
+        mask += 1
+        unmask = 1 << (32 - mask)
+        ip2 = ip + unmask
+        unmask = (1 << 32) - unmask
+        ips1 = set()
+        ips2 = set()
+        for k in ips:
+            if k & unmask == ip:
+                ips1.add(k)
+            else:
+                assert k & unmask == ip2
+                ips2.add(k)
+        ip1, mask1, coverage1 = shrink_subnet(ips1, ip, mask)
+        ip2, mask2, coverage2 = shrink_subnet(ips2, ip2, mask)
+        score = coverage1 + coverage2 - coverage - coverage
+        if score > max_score:
+            max_score = score
+            chosen_ip = ip
+            chosen_ip1, chosen_mask1 = ip1, mask1
+            chosen_ip2, chosen_mask2 = ip2, mask2
+    if chosen_ip is None:
+        break
+    r.remove((chosen_ip, rm))
+    r.add((chosen_ip1, chosen_mask1))
+    r.add((chosen_ip2, chosen_mask2))
+    rm = 32
+    for ip, mask in r:
+        if mask < rm:
+            rm = mask
+    if rm == 32:
+        break
+t = {}
+for ip, mask in r:
+    unmask = 1 << (32 - mask)
+    unmask = (1 << 32) - unmask
+    ips = set()
+    for i in xrange(bisect_left(l, ip), len(l)):
+        if l[i] & unmask == ip:
+            ips.add(l[i])
         else:
-            assert abs(penalty - 1. + len(ips) * 1. / (1 << (32 - mask))) < 1e-5
-            d[ip] = mask, count, penalty, ips
-    for k in to_del:
-        del d[k]
-else:
-    for k in d.keys():
-        ip = k
-        mask, count, penalty = d[ip]
-        d[ip] = mask, count, penalty, set()
+            break
+    assert len(ips)
+    coverage = len(ips) * 1. / (1 << (32 - mask))
+    t[ip] = mask, sum([d[k] for k in ips]), 1. - coverage, ips
+d = t
 
 ar = [('%s/%d' % (socket.inet_ntoa(struct.pack('!L', k)), v[0]), v[1], v[2], ' '.join([socket.inet_ntoa(struct.pack('!L', x)) for x in v[3]])) for k, v in d.iteritems()]
 ar.sort(key=lambda x: x[1], reverse=True)
