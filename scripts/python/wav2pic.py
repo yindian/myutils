f38ca403fa53c06c6e1ed441ef3da4a73d8333fd
Yin Dian <yindian@gmail.com>
2018-10-26 13:02:39 +0800

    update wave visualizer

--- scripts/python/wav2pic.py
+++ scripts/python/wav2pic.py
@@ -2,6 +2,7 @@
 import sys, os
 import struct
 import wave
+import math
 from PIL import Image, ImageDraw
 from cStringIO import StringIO
 
@@ -33,19 +34,35 @@ def main(fname_wav, freq=80, fname_pic=None):
     period = int(round(rate * 1. / freq))
     phase = find_phase(ar, period)
     br = []
+    func = sum
+    #func = max
+    #def func(l):
+    #    l.sort()
+    #    return l[len(l) / 2]
+    if phase * 2 <= period:
+        br.append(func(map(abs, ar[:phase])) * period / phase)
+    else:
+        br.append(func(map(abs, ar[:phase] + ar[phase * 2 - period:phase])))
     for i in xrange(phase, len(ar), period):
-        br.append(sum(map(abs, ar[i:i + period])))
+        br.append(func(map(abs, ar[i:i + period])))
     del br[-1]
     m = max(br)
-    step = 10
-    amp = 100
+    step = 16
+    amp = 160
+    #step, amp = [x * 2 for x in (step, amp)]
+    upscale = 8
+    step, amp = [x * upscale for x in (step, amp)]
     ar = [x * amp / m for x in br]
-    img = Image.new('L', (len(ar) * step, amp * 2), 255)
+    img = Image.new('L', (len(ar) * step, amp * 2), 0)
     draw = ImageDraw.Draw(img)
     for i in xrange(len(ar)):
-        w = min(ar[i], step / 2)
-        x = (step - w) / 2
-        draw.rectangle((step * i + x, amp - ar[i], step * (i + 1) - x, amp + ar[i]), 0)
+        w = min(ar[i] * 2, step / 2)
+        x = int(math.ceil((step - w) / 2.))
+        draw.ellipse((step * i + x, amp - ar[i], step * (i + 1) - x, amp + ar[i]), 255)
+    img = img.resize([x / upscale for x in img.size], Image.BILINEAR)
+    mask = img
+    img = Image.new('RGBA', img.size, (222, 210, 115, 0))
+    img.putalpha(mask)
     sf = StringIO()
     img.save(sf, 'PNG')
     if fname_pic:
