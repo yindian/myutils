bbd404ae7751e6e4b6b23766f7e08b4be04c5fa1
Yin Dian <yindian@gmail.com>
2018-10-29 19:00:45 +0800

    update wave visualizer to make reversed reversed = reversed

--- scripts/python/wav2pic.py
+++ scripts/python/wav2pic.py
@@ -46,23 +46,23 @@ def main(fname_wav, freq=80, fname_pic=None):
     mm = max(map(abs, ar))
     func = sum
     #func = max
-    #def func(l):
-    #    l.sort()
-    #    return l[len(l) / 2]
-    if phase * 2 <= period:
+    if not phase:
+        pass
+    elif phase * 2 <= period:
         br.append(func(map(abs, ar[:phase])) * period / phase)
     else:
         br.append(func(map(abs, ar[:phase] + ar[phase * 2 - period:phase])))
     for i in xrange(phase, len(ar), period):
         br.append(func(map(abs, ar[i:i + period])))
-    del br[-1]
+    if phase:
+        del br[-1]
     m = max(br)
     step = 16
     amp = 160
     #step, amp = [x * 2 for x in (step, amp)]
     upscale = 8
     step, amp = [x * upscale for x in (step, amp)]
-    ar = [x * amp / m for x in br]
+    ar = [int(round(1. * x * amp / m)) for x in br]
     img = Image.new('L', (len(ar) * step, amp * 2), 0)
     draw = ImageDraw.Draw(img)
     for i in xrange(len(ar)):
@@ -80,12 +80,57 @@ def main(fname_wav, freq=80, fname_pic=None):
             f.write(sf.getvalue())
     else:
         sys.stdout.write(sf.getvalue())
+    print >> sys.stderr, ar
     root, ext = os.path.splitext(fname_wav)
     br = []
     for x in ar:
-        peak = x * mm / amp
+        peak = x * m * math.pi / (period * amp * 2.)
+        #peak = x * mm / amp
+        cr = []
         for i in xrange(period):
-            br.append(int(round(peak * math.sin(math.pi * i / period))))
+            cr.append(peak * math.sin(math.pi * 2. * i / period))
+        dr = map(int, map(round, cr))
+        er = map(int, cr)
+        assert func(map(abs, er)) <= x * m / amp
+        diff = func(map(abs, dr)) - x * m / amp
+        if func == sum:
+            if diff > 0:
+                while diff > 0:
+                    mn = 1.
+                    k = -1
+                    for i in xrange(period):
+                        if dr[i] != er[i]:
+                            assert abs(dr[i] - er[i]) == 1
+                            d = abs(cr[i] - (dr[i] + er[i]) / 2.)
+                            if d < mn:
+                                mn = d
+                                k = i
+                    assert k >= 0
+                    if dr[k] > 0:
+                        assert dr[k] == er[k] + 1
+                    else:
+                        assert dr[k] == er[k] - 1
+                    dr[k] = er[k]
+                    diff -= 1
+            elif diff < 0:
+                while diff < 0:
+                    mx = 0.
+                    k = -1
+                    for i in xrange(period):
+                        if dr[i] == er[i]:
+                            d = abs(cr[i] - dr[i])
+                            if d > mx:
+                                mx = d
+                                k = i
+                    assert k >= 0
+                    assert cr[k] != 0.
+                    if cr[k] > 0:
+                        dr[k] += 1
+                    else:
+                        dr[k] -= 1
+                    diff += 1
+            assert func(map(abs, dr)) == x * m / amp
+        br.extend(dr)
     store_samples(root + '-xform' + ext, br, rate)
 
 if __name__ == '__main__':
